ef11faf2477f8622e367b878265a4fd8e52877e1
shruthiasa524 <shruthiasa52@gmail.com>
2025-12-17 15:49:16 +0530

    Add files via upload

---
+++ employe2.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import sqlite3
+import pandas as pd
+###########################
+conn=sqlite3.connect("emp.db",check_same_thread=False)
+cursor=conn.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS employee
+(id integer,name text,sal integer)""")
+conn.commit()
+##############################
+menu=["INSERT","VIEW"]
+choice=st.sidebar.selectbox("MENU",menu)
+if choice=="INSERT":
+  eid=st.number_input("ID")
+  name=st.text_input("NAME")
+  sal=st.number_input("SALARY")
+  if st.button("SAVE"):
+      cursor.execute("""INSERT INTO employee(id,name,sal)
+      VALUES(?,?,?)""",(eid,name,sal))
+      conn.commit()
+      st.success("EMPLOYEE ADDED SUCCESSFULLY")
+####################################################
+if choice=="VIEW":
+    data=cursor.execute("SELECT * FROM employee")
+    st.dataframe(data)
+    
+    
